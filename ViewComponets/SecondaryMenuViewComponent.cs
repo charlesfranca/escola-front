0edccae745c29f8814d49c16de0f1426dcbedb03
charlesfranca <charles-franca@live.com>
2017-01-27 15:38:52 -0200

    ajuste no nome do menu escola da escolete

---
+++ ViewComponets/SecondaryMenuViewComponent.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EscolaDeVoce.Frontend.ViewComponents
+{
+    public class SecondaryMenuViewComponent : ViewComponent
+    {
+        protected string getClaimValue(string name){
+            var identity = (ClaimsIdentity) User.Identity;
+            var value = identity.Claims.Where(c => c.Type == name)
+                    .Select(c => c.Value).SingleOrDefault();
+
+            return value;
+        }
+
+        protected void setClaimValue(string name, string value){
+            var identity = (ClaimsIdentity) User.Identity;
+            var claim = identity.Claims.Where(c => c.Type == name).SingleOrDefault();
+
+            if(claim != null){
+                identity.RemoveClaim(claim);
+            }
+
+            identity.AddClaim(new Claim(name, value));
+
+            var userPrincipal = new ClaimsPrincipal(identity);
+        }
+
+        public IViewComponentResult Invoke()
+        {   
+            var names = getClaimValue(ClaimTypes.Name).Split(' ');
+            var model = new User {
+                name = names[0]
+            };
+            return View(model);
+        }
+
+    }
+}
